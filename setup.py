155c396fca509ee632d08cfcdebed1a63ff64256
Ivan Rychkov <rychkov.ivan@gmail.com>
2020-08-01 15:53:48 +0300

    roc-auc import
    v0.0.2

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ds_helpers-ivanrychkov", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="IvanRychkov",
     author_email="rychyrych@yandex.ru",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/ivanrychkov/helpers",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
