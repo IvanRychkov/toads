fa990f7d37c4b20116f12ec0deea4d00498f9a27
Ivan Rychkov <rychkov.ivan@gmail.com>
2020-08-01 15:58:48 +0300

    pandas import

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -5,6 +5,7 @@ from matplotlib.ticker import PercentFormatter
 from itertools import chain
 from collections import defaultdict
 import numpy as np
+import pandas as pd
 from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score, roc_auc_score
 
 
