ce5ae33fdbc773cec3a78c63155c6579ca39320c
Ivan Rychkov <rychyrych@yandex.com>
2020-09-24 22:20:16 +0300

    __init__ edited

--- toads/__init__.py
+++ toads/__init__.py
@@ -1,6 +1,6 @@
 """This package contains functions and classes TO Assist in Data Science."""
 
 __all__ = ['eda', 'image', 'ml', 'preprocessing',
-           'RandomCipher', 'stats', 'sql', 'utils']
+           'RandomCipher', 'stats', 'sql', 'text', 'utils']
 
 from . import *
