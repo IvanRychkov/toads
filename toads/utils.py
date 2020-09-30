c2d9f27a731b717e98600974930d39c0eb81e2f1
Ivan Rychkov <rychyrych@yandex.com>
2020-09-30 15:17:46 +0300

    dict_agg added to utils.py

--- toads/utils.py
+++ toads/utils.py
@@ -1,8 +1,15 @@
 import numpy as np
+from collections.abc import Iterable
 
 
 def float_equal(a, b, threshold=1e-6):
     return np.abs(a - b) < threshold
 
 
-__all__ = ['float_equal']
+def dict_agg(d, agg_func=np.mean):
+    """Вызывает функцию агрегации для каждого элемента словаря."""
+    return {k: agg_func(v) if issubclass(v.__class__, Iterable) and not isinstance(v, str) else v
+            for k, v in d.items()}
+
+
+__all__ = ['dict_agg', 'float_equal']
