eddf9554c9f8ab3b38943b488ca0d35fa49548f6
Ivan Rychkov <rychyrych@yandex.com>
2020-11-05 23:44:12 +0300

    refactored

--- toads/stats.py
+++ toads/stats.py
@@ -1,6 +1,6 @@
 from scipy.stats import mannwhitneyu
 
-from .image import Image
+from .image import Img
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -33,7 +33,7 @@ def dist_compare(true, preds, hypothesis=True, image='dist', **img_kws):
     """Рисует распределения ответов модели и реальных значений целевого признака.
     Проверяет сходство выборок критерием Манна-Уитни."""
     if image:
-        with Image(legend='a' if image == 'dist' else None, **img_kws):
+        with Img(legend='a' if image == 'dist' else None, **img_kws):
             if image == 'dist':
                 sns.distplot(true, label='true')
                 sns.distplot(preds, label='preds')
