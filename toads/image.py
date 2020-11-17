11fa47e51a7da06f0325505c80cfacf80d42892f
Ivan Rychkov <rychyrych@yandex.com>
2020-11-17 20:02:51 +0300

    rethinking Img to better support time series

--- toads/image.py
+++ toads/image.py
@@ -44,7 +44,9 @@ class Img:
 
         if len(plt.gcf().axes) != 0:
             if (self.ts_data is not None) and self.ts_n_ticks:
-                Img.time_series_format(self.ts_data, self.ts_n_ticks, self.ts_autofmt_x)
+                Img.time_series_format(date_col=self.ts_data,
+                                       n_ticks=self.ts_n_ticks,
+                                       autofmt_x=self.ts_autofmt_x)
             if self.tight:
                 plt.tight_layout(pad=2.5)
             if self.st:
