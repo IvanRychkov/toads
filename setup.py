4425487f2ce70dab0cba064485b402bd12ba081d
Ivan Rychkov <rychyrych@yandex.com>
2020-11-21 04:55:01 +0300

    removed timeseries from Img, added plot_time_series to eda.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="toads-ivanrychkov",
-    version="0.0.9",
+    version="0.0.10",
     author="Ivan Rychkov",
     author_email="rychyrych@yandex.ru",
     description="Data Science tools from preprocessing and visualization to statistics and ML.",
