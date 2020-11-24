74aa8c409172aba682ef26546e633a7f3fb9e0bd
Ivan Rychkov <rychyrych@yandex.com>
2020-11-25 02:20:18 +0300

    added SQLContext in sql module for working locally with comfort

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="toads-ivanrychkov",
-    version="0.0.11",
+    version="0.0.12",
     author="Ivan Rychkov",
     author_email="rychyrych@yandex.ru",
     description="Data Science tools from preprocessing and visualization to statistics and ML.",
