eb2a47352c70d1b97cf0d7f0868fa59f85b0ee17
Ivan Rychkov <rychkov.ivan@gmail.com>
2022-01-28 15:24:37 +0300

    add dependencies to setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="toads-ivanrychkov",
-    version="0.0.21",
+    version="0.0.22",
     author="Ivan Rychkov",
     author_email="rychyrych@yandex.ru",
     description="Data Science tools from preprocessing and visualization to statistics and ML.",
@@ -13,6 +13,13 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ivanrychkov/toads",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'seaborn',
+        'scikit-learn',
+        'numpy',
+        'pandas',
+        'tqdm'
+    ]
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
