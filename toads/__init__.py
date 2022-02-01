8c0130b0b9e307e103646ab756852847a31b642b
Ivan Rychkov <rychkov.ivan@gmail.com>
2022-02-01 23:06:52 +0300

    fix missing comma

--- toads/__init__.py
+++ toads/__init__.py
@@ -1,6 +1,6 @@
 """This package contains functions and classes TO Assist in Data Science."""
 
 __all__ = ['eda', 'etl', 'image', 'ml', 'preprocessing',
-           'RandomCipher', 'redis' 'stats', 'sql', 'utils']
+           'RandomCipher', 'redis', 'stats', 'sql', 'utils']
 
 from . import *
