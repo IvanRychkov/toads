0c96d643e271f1628e349f9b6af0d40f8b343c3b
Ivan Rychkov <rychkov.ivan@gmail.com>
2022-05-26 17:12:10 +0300

    fix type assertions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="toads-ivanrychkov",
-    version="0.0.26",
+    version="0.0.27",
     author="Ivan Rychkov",
     author_email="rychyrych@yandex.ru",
     description="Data Science tools from preprocessing and visualization to statistics and ML.",
