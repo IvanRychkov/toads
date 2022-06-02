1f28969b8c64c25ba1f67b2f963179ce2081780b
Ivan Rychkov <rychkov.ivan@gmail.com>
2022-06-02 19:00:57 +0300

    add upsert_into_postgres_table() function factory for upserting values

--- toads/sql.py
+++ toads/sql.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Table
 from sqlalchemy.engine import Engine
 from typing import List
+from sqlalchemy.dialects.postgresql import insert
 
 
 def insert_into_table(table: Table, db: Engine):
@@ -13,4 +14,20 @@ def insert_into_table(table: Table, db: Engine):
     return insert_func
 
 
-__all__ = ['insert_into_table']
+def upsert_into_postgres_table(table: Table, db: Engine, index_elements: List[str],
+                               update_values_on_conflict: List[str]):
+    """Try to insert rows into Postgres table"""
+
+    def upsert_func(data_batch: List[dict]):
+        with db.begin():
+            insert_stmt = insert(table).values(data_batch)
+            stmt = insert_stmt.on_conflict_do_update(
+                index_elements=index_elements,
+                set_={field: insert_stmt.excluded[field] for field in update_values_on_conflict},
+            )
+            stmt.execute()
+
+    return upsert_func
+
+
+__all__ = ['insert_into_table', 'upsert_into_postgres_table']
